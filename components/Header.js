e3e536f9d36a56274fd3b67553986b0d623f0e78
hungdoba <hungdoba.hdb@gmail.com>
2022-06-02 19:57:19 +0900

    asdfasD

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,12 @@
-import Link from "next/link";
 function Header() {
   return (
-    <div className="top">
-      <div className="row large light-grey">
-        <div className="topbar">
-          <div className="col margin">
-            <Link href="/" style={{ textDecoration: "none" }}>
-              Hyip-Review.com
-            </Link>
-          </div>
-          {/* <div className="col s3">
-            <a href="/all" className="button block">
-              All project
-            </a>
-          </div>
-          <div className="col s3">
-            <a href="/" className="button block">
-              About us
-            </a>
-          </div> */}
-        </div>
+    <div className="title">
+      <div className="title-content">
+        <ul class="nav">
+          <li>
+            <a href="/">Hyip-review.com </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
